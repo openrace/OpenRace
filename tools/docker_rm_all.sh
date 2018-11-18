e1f0a2245d428d5d747f99b82ba9efe849aca463
Marc Urben <aegnor@mittelerde.ch>
2018-11-18 17:05:37 +0100

    adding some messages

--- tools/docker_rm_all.sh
+++ tools/docker_rm_all.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # Stop all containers
+echo "Stopping all containers..."
 docker stop $(docker ps -a -q)
 # Delete all containers
+echo "Deleting all containers..."
 docker rm $(docker ps -a -q)
 # Delete all images
+echo "Deleting all images..."
 docker rmi $(docker images -q)
