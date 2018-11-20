2d20026cc2add8944461569359184226ca990f53
David Vogt <david.vogt@adfinis-sygroup.ch>
2018-11-20 16:15:48 +0100

    Various cleanups
    * Use emitter all over the place
    * Remove unneeded / redundant code
    * add click for cli awesomeness
    * add setup.py
    
    Note: to test, run `setup.py install -e .`, then execute `openrace`
    (which should be installed in your virtualenv or where ever)

--- python/race_core/handlers/serialinterface.py
+++ python/race_core/handlers/serialinterface.py
@@ -3,37 +3,26 @@
 
 import logging
 import serial
+from ..common import Emitter
 
 
 class SerialInterfaceHandler:
-    def __init__(self, device = None):
+    def __init__(self, device):
         self.device = device
-        self.buf = bytearray()
         self.serial = None
-
-        if self.device:
-            self.open_device()
-
-    def open_device(self):
-        logging.info("opening serial device %s" % (self.device))
+        self.data_available = Emitter()
         self.serial = serial.Serial(self.device, 115200, timeout=0)
 
-    def readline(self):
-        i = self.buf.find(b"\n")
-        if i >= 0:
-            r = self.buf[:i + 1]
-            self.buf = self.buf[i + 1:]
-            return r
+    def read_data(self, stop_if_no_data=False):
         while True:
-            i = max(1, min(2048, self.serial.in_waiting))
-            data = self.serial.read(i)
-            i = data.find(b"\n")
-            if i >= 0:
-                r = self.buf + data[:i + 1]
-                self.buf[0:] = data[i + 1:]
-                return r
-            else:
-                self.buf.extend(data)
+            readable_len = max(0, min(2048, self.serial.in_waiting))
+            if stop_if_no_data and readable_len == 0:
+                return
+            elif readable_len == 0:
+                readable_len = 1
+            data = self.serial.read(readable_len)
+            if len(data):
+                self.data_available.emit(data)
 
     def send_data(self, data):
         logging.debug("sending data <%s>" % data)
