e6f91ebdf703ac3a5e717e020cd31909af89e269
Marc Urben <aegnor@mittelerde.ch>
2018-11-21 23:57:03 +0100

    implementing basic mqtt things

--- python/race_core/core.py
+++ python/race_core/core.py
@@ -2,9 +2,9 @@
 # vim: set filencoding=utf-8
 
 import logging
-import time
 import os
 import click
+import paho.mqtt.client as mqtt
 
 from .handlers.racetracker import LapRFRaceTracker
 
@@ -18,17 +18,55 @@ logging.basicConfig(
 )
 
 
+class RaceCore:
+    def __init__(self, device, server, user, password):
+        self.mqtt_server = server
+        self.mqtt_user = user
+        self.mqtt_password = password
+        self.client = None
+
+        self.tracker = LapRFRaceTracker(device)
+        self.tracker.on_version.connect(logging.info)
+
+    def mqtt_connect(self):
+        logging.info("Connecting to MQTT server %s" % (self.mqtt_server))
+        self.client = mqtt.Client()
+
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+
+        self.client.username_pw_set(self.mqtt_user, self.mqtt_password)
+
+        self.client.connect(self.mqtt_server, 1883, 60)
+
+    def on_connect(self, client, userdata, flags, rc):
+        logging.info("Sucessfully connected to MQTT server with result code" + str(rc))
+
+        #self.client.subscribe("$SYS/#")
+        self.client.subscribe("/OpenRace/events")
+        # self.client.message_callback_add("/d1ws2812/discovery", self.on_discovery_message)
+
+        self.client.subscribe("/openrace/pilots")
+        # self.client.message_callback_add("/openrace/race/start", self.on_race_start)
+
+    def on_message(self, client, userdata, msg):
+        logging.debug("Recieved MQTT message: <%s> <%s>" % (msg.topic, msg.payload))
+
+    def run(self):
+        self.tracker.request_version()
+        while True:
+            self.client.loop()
+            self.tracker.read_data(stop_if_no_data=True)
+
+
 @click.command()
 @click.option('--device', prompt='Device?', default="/dev/ttyACM0")
 def main(device):
     logging.info("starting up")
 
-    tracker = LapRFRaceTracker(device)
-    tracker.on_version.connect(logging.info)
-
-    tracker.request_version()
-
-    tracker.read_data(stop_if_no_data=True)
+    rc = RaceCore(device, "10.5.20.35", "openrace", "PASSWORD")
+    rc.mqtt_connect()
+    rc.run()
 
 
 if __name__ == '__main__':
