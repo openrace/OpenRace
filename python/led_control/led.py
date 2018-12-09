10731ba75944255efb725f8e30306837378d0bd9
Marc Urben <aegnor@mittelerde.ch>
2018-12-09 22:42:09 +0100

    Pilot passing is now working

--- python/led_control/led.py
+++ python/led_control/led.py
@@ -53,6 +53,9 @@ class LedController:
         self.client.subscribe("/OpenRace/events/#")
         self.client.message_callback_add("/OpenRace/events/start", self.on_race_start)
 
+        self.client.subscribe("/OpenRace/race/#")
+        self.client.message_callback_add("/OpenRace/race/passing/#", self.on_pilot_passing)
+
     def on_discovery_message(self, client, userdata, msg):
         client_mac = msg.topic.split("/")[-1]
         client_version = msg.payload.decode("utf-8")
@@ -60,6 +63,48 @@ class LedController:
         if client_mac not in self.led_strips.keys():
             self.client.publish("/d1ws2812/%s" % client_mac, "6;255;0;0", qos=1)
         self.led_strips[client_mac] = time.time()
+
+    def on_pilot_passing(self, client, userdata, msg):
+        frequency = int(msg.payload)
+        logging.debug("Pilot with frequency %s passed the gate" % frequency)
+        # https://github.com/betaflight/betaflight/blob/39ced6bbfefa52a6f605ed6635b7e62105c71672/src/main/io/ledstrip.c
+
+
+        # } else if (frequency <= 5750) {
+        #     colorIndex = COLOR_ORANGE;
+        # } else if (frequency <= 5789) {
+        #     colorIndex = COLOR_YELLOW;
+        # } else if (frequency <= 5829) {
+        #     colorIndex = COLOR_GREEN;
+        # } else if (frequency <= 5867) {
+        #     colorIndex = COLOR_BLUE;
+        # } else if (frequency <= 5906) {
+        #     colorIndex = COLOR_DARK_VIOLET;
+        # } else {
+        #     colorIndex = COLOR_DEEP_PINK;
+        # }
+        # hsvColor_t color = ledStripConfig()->colors[colorIndex];
+        # color.v = pit ? (blink ? 15 : 0) : 255; // blink when in pit mode
+        # applyLedHsv(LED_MOV_OVERLAY(LED_FLAG_OVERLAY(LED_OVERLAY_VTX)), &color);
+
+        color = "255;20;147"        # COLOR_DEEP_PINK
+        if frequency <= 5672:
+            color = "255;255;255"   # COLOR_WHITE
+        elif frequency <= 5711:
+            color = "255;0;0"       # COLOR_RED
+        elif frequency <= 5750:
+            color = "255;165;0"     # COLOR_ORANGE
+        elif frequency <= 5789:
+            color = "255;255;0"     # COLOR_YELLOW
+        elif frequency <= 5829:
+            color = "0;255;0"       # COLOR_GREEN
+        elif frequency <= 5867:
+            color = "0;0;255"       # COLOR_BLUE
+        elif frequency <= 5906:
+            color = "238;130;238"   # COLOR_DARK_VIOLET
+
+        self.client.publish("/d1ws2812/all", "6;%s" % color, qos=1)
+
 
     def on_message(self, client, userdata, msg):
         logging.debug("Recieved MQTT message: <%s> <%s>" % (msg.topic, msg.payload))
