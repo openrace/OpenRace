bdea9beaef285cae8c38188179e8208fb0cd8f19
Roman Scheidegger <romanscheidegger@gmail.com>
2018-12-26 21:34:47 +0100

    some basic restylings

--- python/angular-ui/src/app/app.module.ts
+++ python/angular-ui/src/app/app.module.ts
@@ -3,9 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MatCardModule, MatFormFieldModule, MatTabsModule, MatToolbarModule} from "@angular/material";
+import {
+  MatButtonModule,
+  MatCardModule, MatDividerModule,
+  MatExpansionModule,
+  MatFormFieldModule, MatInputModule, MatSelectModule,
+  MatTabsModule,
+  MatToolbarModule
+} from "@angular/material";
 import { PilotsComponent } from './pilots/pilots.component';
 import {IMqttServiceOptions, MqttModule} from "ngx-mqtt";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {DragDropModule} from "@angular/cdk/drag-drop";
 
 export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
   hostname: '10.5.20.35',
@@ -20,13 +29,20 @@ export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
     PilotsComponent
   ],
   imports: [
+    FlexLayoutModule,
     MqttModule.forRoot(MQTT_SERVICE_OPTIONS),
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatTabsModule,
     MatCardModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatExpansionModule,
+    MatDividerModule,
+    MatButtonModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
