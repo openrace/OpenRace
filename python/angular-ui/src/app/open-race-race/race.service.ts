87abe070f9576a5312fd85bc5b2be58b53b56d10
Roman Scheidegger <romanscheidegger@gmail.com>
2019-01-06 20:59:50 +0100

    initial race service

---
+++ python/angular-ui/src/app/open-race-race/race.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { MessageQueueClientService } from '../open-race-common/message-queue-client.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RaceService {
+
+  constructor(private messageQueueClientService: MessageQueueClientService) {
+  }
+
+  startRace() {
+    this.messageQueueClientService.publishToTopicUnretained('/OpenRace/race/start', '1');
+  }
+
+  stopRace() {
+    this.messageQueueClientService.publishToTopicUnretained('/OpenRace/race/stop', '1');
+  }
+
+  setMinLapTime(minLapTimeInSeconds: number) {
+    this.messageQueueClientService
+      .publishToTopicUnretained('/OpenRace/race/settings/min_lap_time_in_seconds', minLapTimeInSeconds.toString());
+  }
+
+  setStartDelay(startDelayInSeconds: number) {
+    this.messageQueueClientService
+      .publishToTopicUnretained('/OpenRace/race/settings/start_delay_in_seconds', startDelayInSeconds.toString());
+  }
+
+  setAmountOfLaps(amountOfLaps: number) {
+    this.messageQueueClientService.publishToTopicUnretained('/OpenRace/race/settings/amount_laps', amountOfLaps.toString());
+  }
+}
