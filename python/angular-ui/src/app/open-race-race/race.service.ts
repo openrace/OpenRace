b35dc0638314d5674f123c978e0318a85edd91b7
Roman Scheidegger <romanscheidegger@gmail.com>
2019-01-07 21:29:37 +0100

    send settings retained

--- python/angular-ui/src/app/open-race-race/race.service.ts
+++ python/angular-ui/src/app/open-race-race/race.service.ts
@@ -19,19 +19,19 @@ export class RaceService {
 
   setMinLapTime(minLapTimeInSeconds: number) {
     this.messageQueueClientService
-      .publishToTopicUnretained('/OpenRace/race/settings/min_lap_time_in_seconds', minLapTimeInSeconds.toString());
+      .publishToTopic('/OpenRace/race/settings/min_lap_time_in_seconds', minLapTimeInSeconds.toString());
   }
 
   setStartDelay(startDelayInSeconds: number) {
     this.messageQueueClientService
-      .publishToTopicUnretained('/OpenRace/race/settings/start_delay_in_seconds', startDelayInSeconds.toString());
+      .publishToTopic('/OpenRace/race/settings/start_delay_in_seconds', startDelayInSeconds.toString());
   }
 
   setAmountOfLaps(amountOfLaps: number) {
-    this.messageQueueClientService.publishToTopicUnretained('/OpenRace/race/settings/amount_laps', amountOfLaps.toString());
+    this.messageQueueClientService.publishToTopic('/OpenRace/race/settings/amount_laps', amountOfLaps.toString());
   }
 
   setRaceMw(raceMw: number) {
-    this.messageQueueClientService.publishToTopicUnretained('/OpenRace/race/settings/race_mw', raceMw.toString());
+    this.messageQueueClientService.publishToTopic('/OpenRace/race/settings/race_mw', raceMw.toString());
   }
 }
