4f01d98afb2343563845fe2cbc1991c2383b9de1
Marc Urben <aegnor@mittelerde.ch>
2019-11-29 17:03:04 +0100

    support enabling and disabling of the rpi access point

---
+++ ansible/ap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z ${1+x} ]; then
+  echo "Please supply true or false to enable or disable the Raspberry Pi access point."
+  exit 1
+elif [ "$1" = true ]; then
+  echo "Enabling Raspberry Pi wifi access point"
+  OPTION="{\"raspberry_ap\": true}"
+else
+  echo "Disabling Raspberry Pi wifi access point"
+  OPTION="{\"raspberry_ap\": false}"
+fi
+
+ansible-playbook site.yml -e "${OPTION}" --tags ap
