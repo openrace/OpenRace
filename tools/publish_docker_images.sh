41f8ee57fb2622d94fa33e0bb557055cd259722b
Roman Scheidegger <roman.scheidegger@gmail.com>
2020-06-14 07:41:20 +0200

    adjust tag to include registry

--- tools/publish_docker_images.sh
+++ tools/publish_docker_images.sh
@@ -6,9 +6,9 @@ IMAGE_VERSION="1.0.3"
 
 for SERVICE in race_core led_control audio_output ui
 do
-    cat ../src/${SERVICE}/Dockerfile | sed '/as base/ s/FROM /FROM amd64\//g' | docker buildx build --platform linux/amd64 -f - -t openrace/${SERVICE}:${IMAGE_VERSION}-amd64 ../src/${SERVICE}/
+    cat ../src/${SERVICE}/Dockerfile | sed '/as base/ s/FROM /FROM amd64\//g' | docker buildx build --platform linux/amd64 -f - -t docker.io/openrace/${SERVICE}:${IMAGE_VERSION}-amd64 ../src/${SERVICE}/
     docker push docker.io/openrace/${SERVICE}:${IMAGE_VERSION}-amd64
 
-    cat ../src/${SERVICE}/Dockerfile | sed '/as base/ s/FROM /FROM arm32v7\//g' | docker buildx build --platform linux/arm/v7 -f - -t openrace/${SERVICE}:${IMAGE_VERSION}-arm32v7 ../src/${SERVICE}/
+    cat ../src/${SERVICE}/Dockerfile | sed '/as base/ s/FROM /FROM arm32v7\//g' | docker buildx build --platform linux/arm/v7 -f - -t docker.io/openrace/${SERVICE}:${IMAGE_VERSION}-arm32v7 ../src/${SERVICE}/
     docker push docker.io/openrace/${SERVICE}:${IMAGE_VERSION}-arm32v7
 done
