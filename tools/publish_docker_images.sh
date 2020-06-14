b55a5100e2b78f907a27c3419b2f3fb3a432eca9
Roman Scheidegger <roman.scheidegger@gmail.com>
2020-06-14 06:52:23 +0200

    switch to buildx for multi platform building

--- tools/publish_docker_images.sh
+++ tools/publish_docker_images.sh
@@ -2,16 +2,13 @@
 
 set -e
 
-IMAGE_VERSION="1.0.2"
+IMAGE_VERSION="1.0.3"
 
 for SERVICE in race_core led_control audio_output ui
 do
-    cat ../src/${SERVICE}/Dockerfile | sed '/as base/ s/FROM /FROM amd64\//g' | docker build -f - -t openrace/${SERVICE}:${IMAGE_VERSION}-amd64 ../src/${SERVICE}/
+    cat ../src/${SERVICE}/Dockerfile | sed '/as base/ s/FROM /FROM amd64\//g' | docker build --platform linux/amd64 -f - -t openrace/${SERVICE}:${IMAGE_VERSION}-amd64 ../src/${SERVICE}/
     docker push openrace/${SERVICE}:${IMAGE_VERSION}-amd64
 
-    # https://github.com/gliderlabs/docker-alpine/issues/298
-    # https://github.com/gliderlabs/docker-alpine/pull/484
-    # thanks alpine linux for arm32v6 -.-
-    cat ../src/${SERVICE}/Dockerfile | sed '/as base/ s/FROM /FROM arm32v7\//g' | docker build -f - -t openrace/${SERVICE}:${IMAGE_VERSION}-arm32v7 ../src/${SERVICE}/
+    cat ../src/${SERVICE}/Dockerfile | sed '/as base/ s/FROM /FROM arm32v7\//g' | docker buildx build --platform linux/arm/v7 -f - -t openrace/${SERVICE}:${IMAGE_VERSION}-arm32v7 ../src/${SERVICE}/
     docker push openrace/${SERVICE}:${IMAGE_VERSION}-arm32v7
 done
