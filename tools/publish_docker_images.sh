ea8fa0fe2eb0f3fb81a8f4b49baea1dd6321479d
Roman Scheidegger <roman.scheidegger@gmail.com>
2020-06-14 07:35:19 +0200

    make docker registry explicit

--- tools/publish_docker_images.sh
+++ tools/publish_docker_images.sh
@@ -7,8 +7,8 @@ IMAGE_VERSION="1.0.3"
 for SERVICE in race_core led_control audio_output ui
 do
     cat ../src/${SERVICE}/Dockerfile | sed '/as base/ s/FROM /FROM amd64\//g' | docker buildx build --platform linux/amd64 -f - -t openrace/${SERVICE}:${IMAGE_VERSION}-amd64 ../src/${SERVICE}/
-    docker push openrace/${SERVICE}:${IMAGE_VERSION}-amd64
+    docker push docker.io/openrace/${SERVICE}:${IMAGE_VERSION}-amd64
 
     cat ../src/${SERVICE}/Dockerfile | sed '/as base/ s/FROM /FROM arm32v7\//g' | docker buildx build --platform linux/arm/v7 -f - -t openrace/${SERVICE}:${IMAGE_VERSION}-arm32v7 ../src/${SERVICE}/
-    docker push openrace/${SERVICE}:${IMAGE_VERSION}-arm32v7
+    docker push docker.io/openrace/${SERVICE}:${IMAGE_VERSION}-arm32v7
 done
