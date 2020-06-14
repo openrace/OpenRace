9e87bd194c914ff50397d9f7c5d9567bcc74be01
Roman Scheidegger <roman.scheidegger@gmail.com>
2020-06-14 08:02:04 +0200

    push from build

--- tools/publish_docker_images.sh
+++ tools/publish_docker_images.sh
@@ -6,9 +6,7 @@ IMAGE_VERSION="1.0.3"
 
 for SERVICE in race_core led_control audio_output ui
 do
-    cat ../src/${SERVICE}/Dockerfile | sed '/as base/ s/FROM /FROM amd64\//g' | docker buildx build --platform linux/amd64 -f - -t docker.io/openrace/${SERVICE}:${IMAGE_VERSION}-amd64 ../src/${SERVICE}/
-    docker push docker.io/openrace/${SERVICE}:${IMAGE_VERSION}-amd64
+    cat ../src/${SERVICE}/Dockerfile | sed '/as base/ s/FROM /FROM amd64\//g' | docker buildx build --platform linux/amd64 -f - -t docker.io/openrace/${SERVICE}:${IMAGE_VERSION}-amd64 --push ../src/${SERVICE}/
 
-    cat ../src/${SERVICE}/Dockerfile | sed '/as base/ s/FROM /FROM arm32v7\//g' | docker buildx build --platform linux/arm/v7 -f - -t docker.io/openrace/${SERVICE}:${IMAGE_VERSION}-arm32v7 ../src/${SERVICE}/
-    docker push docker.io/openrace/${SERVICE}:${IMAGE_VERSION}-arm32v7
+    cat ../src/${SERVICE}/Dockerfile | sed '/as base/ s/FROM /FROM arm32v7\//g' | docker buildx build --platform linux/arm/v7 -f - -t docker.io/openrace/${SERVICE}:${IMAGE_VERSION}-arm32v7 --push ../src/${SERVICE}/
 done
